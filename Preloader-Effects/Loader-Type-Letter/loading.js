d96d35f8426e1b9f03265b3e5a822a8ac1e649bd
imniladri <jnvniladri101@gmail.com>
2022-09-28 13:38:03 +0530

    Fixed Loader Preview

--- Preloader-Effects/Loader-Type-Letter/loading.js
+++ Preloader-Effects/Loader-Type-Letter/loading.js
@@ -5,26 +5,3 @@ var preloader = document.getElementById("preloader");
 window.addEventListener('load', function(){
 	preloader.style.transform = 'translateY(-100%)';
 })
-
-
-
-
-// Animation for Text (anime.js)
-
-var textWrapper = document.querySelector('.loader .letters');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>"); // Each Letter Wrapped in a SPAN
-
-anime.timeline({loop: true})
-  .add({
-    targets: '.loader .letter',
-    scale: [0, 1],
-    duration: 1500,
-    elasticity: 600,
-    delay: (el, i) => 45 * (i+1)
-  }).add({
-    targets: '.loader',
-    opacity: 0,
-    duration: 300,
-    easing: "easeOutExpo",
-    delay: 300
-  });
